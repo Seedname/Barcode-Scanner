d75b9fc9e5ced5e3b625bdcc7ce602fdcb831f32
Seedname <juliandominguez06@icloud.com>
2024-04-11 09:06:05 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,20 @@
-# importing the requests library
 import requests
- 
-# api-endpoint
-ITEM_ID = "720825548692"
-URL = f"https://api.upcdatabase.org/product/{ITEM_ID}"
- 
-# sending get request and saving the response as response object
-r = requests.get(url = URL, headers={"Authorization": "Bearer E13B280796487261E5D32B9DEAF51B30"})
- 
-# extracting data in json format
-data = r.json()
- 
-print(data)
 
+
+def search_item(item_id: str) -> dict:
+    # API link
+    url = f"https://api.upcdatabase.org/product/{item_id}"
+
+    # sending get request and saving the response as response object
+    r = requests.get(url=url, headers={"Authorization": "Bearer E13B280796487261E5D32B9DEAF51B30"})
+
+    # extracting data in json format
+    data = r.json()
+
+    return data
+
+
+if __name__ == "__main__":
+    # Add the code for the barcode scanner here.
+    item_barcode = "720825548692"
+    search_item(item_barcode)
